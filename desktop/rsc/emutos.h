3d87bf7e37d23018370d962b7878db6d3f85e5fe
Davide <bresolin.davide@gmail.com>
2021-12-28 12:32:29 +0100

    Directory with resource file to merge

---
+++ desktop/rsc/emutos.h
@@ -0,0 +1,295 @@
+#define ADMENU 0  	/* TREE */
+#define DESKMENU 3  	/* OBJECT in TREE #0 */
+#define FILEMENU 4  	/* OBJECT in TREE #0 */
+#define VIEWMENU 5  	/* OBJECT in TREE #0 */
+#define OPTNMENU 6  	/* OBJECT in TREE #0 */
+#define ABOUITEM 9  	/* OBJECT in TREE #0 */
+#define OPENITEM 18  	/* OBJECT in TREE #0 */
+#define SHOWITEM 19  	/* OBJECT in TREE #0 */
+#define SRCHITEM 20  	/* OBJECT in TREE #0 */
+#define NFOLITEM 22  	/* OBJECT in TREE #0 */
+#define CLOSITEM 23  	/* OBJECT in TREE #0 */
+#define CLSWITEM 24  	/* OBJECT in TREE #0 */
+#define BTOPITEM 25  	/* OBJECT in TREE #0 */
+#define SLCTITEM 26  	/* OBJECT in TREE #0 */
+#define MASKITEM 27  	/* OBJECT in TREE #0 */
+#define DELTITEM 29  	/* OBJECT in TREE #0 */
+#define FORMITEM 30  	/* OBJECT in TREE #0 */
+#define SEP_FL2 31  	/* OBJECT in TREE #0 */
+#define CLIITEM 32  	/* OBJECT in TREE #0 */
+#define QUITITEM 33  	/* OBJECT in TREE #0 */
+#define ICONITEM 35  	/* OBJECT in TREE #0 */
+#define TEXTITEM 36  	/* OBJECT in TREE #0 */
+#define NAMEITEM 38  	/* OBJECT in TREE #0 */
+#define TYPEITEM 39  	/* OBJECT in TREE #0 */
+#define SIZEITEM 40  	/* OBJECT in TREE #0 */
+#define DATEITEM 41  	/* OBJECT in TREE #0 */
+#define NSRTITEM 42  	/* OBJECT in TREE #0 */
+#define SEP_VW1 43  	/* OBJECT in TREE #0 */
+#define FITITEM 44  	/* OBJECT in TREE #0 */
+#define SEP_VW2 45  	/* OBJECT in TREE #0 */
+#define BACKGRND 46  	/* OBJECT in TREE #0 */
+#define IICNITEM 48  	/* OBJECT in TREE #0 */
+#define IAPPITEM 49  	/* OBJECT in TREE #0 */
+#define IDSKITEM 50  	/* OBJECT in TREE #0 */
+#define RICNITEM 51  	/* OBJECT in TREE #0 */
+#define PREFITEM 53  	/* OBJECT in TREE #0 */
+#define CONFITEM 54  	/* OBJECT in TREE #0 */
+#define RESITEM 55  	/* OBJECT in TREE #0 */
+#define SEP_OP0 56  	/* OBJECT in TREE #0 */
+#define READITEM 57  	/* OBJECT in TREE #0 */
+#define SAVEITEM 58  	/* OBJECT in TREE #0 */
+#define SEP_OP1 59  	/* OBJECT in TREE #0 */
+#define BLITITEM 60  	/* OBJECT in TREE #0 */
+#define SEP_OP2 61  	/* OBJECT in TREE #0 */
+#define CACHITEM 62  	/* OBJECT in TREE #0 */
+#define ADFFINFO 1  	/* TREE */
+#define FFTITLE 1  	/* OBJECT in TREE #1 */
+#define FFNAME 2  	/* OBJECT in TREE #1 */
+#define FFSIZE 3  	/* OBJECT in TREE #1 */
+#define FFDATE 4  	/* OBJECT in TREE #1 */
+#define FFTIME 5  	/* OBJECT in TREE #1 */
+#define FFNUMFOL 6  	/* OBJECT in TREE #1 */
+#define FFNUMFIL 7  	/* OBJECT in TREE #1 */
+#define FFRWRITE 9  	/* OBJECT in TREE #1 */
+#define FFRONLY 10  	/* OBJECT in TREE #1 */
+#define FFOK 11  	/* OBJECT in TREE #1 */
+#define FFSKIP 12  	/* OBJECT in TREE #1 */
+#define FFCNCL 13  	/* OBJECT in TREE #1 */
+#define ADDISKIN 2  	/* TREE */
+#define DITITLE 1  	/* OBJECT in TREE #2 */
+#define DIDRIVE 2  	/* OBJECT in TREE #2 */
+#define DIVOLUME 3  	/* OBJECT in TREE #2 */
+#define DINFOLDS 4  	/* OBJECT in TREE #2 */
+#define DINFILES 5  	/* OBJECT in TREE #2 */
+#define DIUSED 6  	/* OBJECT in TREE #2 */
+#define DIAVAIL 7  	/* OBJECT in TREE #2 */
+#define DIOK 8  	/* OBJECT in TREE #2 */
+#define DICNCL 9  	/* OBJECT in TREE #2 */
+#define ADDINFO 3  	/* TREE */
+#define DEICON 1  	/* OBJECT in TREE #3 */
+#define DELABEL 4  	/* OBJECT in TREE #3 */
+#define DEVERSN 5  	/* OBJECT in TREE #3 */
+#define DECOPYRT 7  	/* OBJECT in TREE #3 */
+#define DEOK 16  	/* OBJECT in TREE #3 */
+#define ADOPENAP 4  	/* TREE */
+#define APPLOPEN 1  	/* OBJECT in TREE #4 */
+#define APPLNAME 2  	/* OBJECT in TREE #4 */
+#define APPLPARM 4  	/* OBJECT in TREE #4 */
+#define APPLOK 5  	/* OBJECT in TREE #4 */
+#define APPLCNCL 6  	/* OBJECT in TREE #4 */
+#define ADINSDSK 5  	/* TREE */
+#define ID_ID 2  	/* OBJECT in TREE #5 */
+#define ID_LABEL 3  	/* OBJECT in TREE #5 */
+#define ID_DRIVE 6  	/* OBJECT in TREE #5 */
+#define ID_TRASH 7  	/* OBJECT in TREE #5 */
+#define ID_PRINT 8  	/* OBJECT in TREE #5 */
+#define ID_IBOX 9  	/* OBJECT in TREE #5 */
+#define ID_ICON 10  	/* OBJECT in TREE #5 */
+#define ID_UP 11  	/* OBJECT in TREE #5 */
+#define ID_DOWN 12  	/* OBJECT in TREE #5 */
+#define ID_OK 14  	/* OBJECT in TREE #5 */
+#define ID_SKIP 15  	/* OBJECT in TREE #5 */
+#define ID_CNCL 16  	/* OBJECT in TREE #5 */
+#define ADINSAPP 6  	/* TREE */
+#define INSAPP 1  	/* OBJECT in TREE #6 */
+#define APNAME 2  	/* OBJECT in TREE #6 */
+#define APARGS 3  	/* OBJECT in TREE #6 */
+#define APDOCTYP 4  	/* OBJECT in TREE #6 */
+#define APFUNKEY 5  	/* OBJECT in TREE #6 */
+#define APAUTO 8  	/* OBJECT in TREE #6 */
+#define APNORM 9  	/* OBJECT in TREE #6 */
+#define APTYPE 10  	/* OBJECT in TREE #6 */
+#define APTOS 12  	/* OBJECT in TREE #6 */
+#define APTTP 13  	/* OBJECT in TREE #6 */
+#define APGEM 14  	/* OBJECT in TREE #6 */
+#define APGTP 15  	/* OBJECT in TREE #6 */
+#define APDEFAPP 18  	/* OBJECT in TREE #6 */
+#define APDEFWIN 19  	/* OBJECT in TREE #6 */
+#define APPMFULL 22  	/* OBJECT in TREE #6 */
+#define APPMFILE 23  	/* OBJECT in TREE #6 */
+#define APINSTAL 24  	/* OBJECT in TREE #6 */
+#define APREMOVE 25  	/* OBJECT in TREE #6 */
+#define APSKIP 26  	/* OBJECT in TREE #6 */
+#define APCANCEL 27  	/* OBJECT in TREE #6 */
+#define ADCPYDEL 7  	/* TREE */
+#define CDTITLE 1  	/* OBJECT in TREE #7 */
+#define CDFOLDS 2  	/* OBJECT in TREE #7 */
+#define CDFILES 3  	/* OBJECT in TREE #7 */
+#define CDOK 4  	/* OBJECT in TREE #7 */
+#define CDCNCL 5  	/* OBJECT in TREE #7 */
+#define ADCPALER 8  	/* TREE */
+#define CACOPY 1  	/* OBJECT in TREE #8 */
+#define CACURRNA 2  	/* OBJECT in TREE #8 */
+#define CACOPYNA 3  	/* OBJECT in TREE #8 */
+#define CAOK 4  	/* OBJECT in TREE #8 */
+#define CASKIP 5  	/* OBJECT in TREE #8 */
+#define CASTOP 6  	/* OBJECT in TREE #8 */
+#define ADMKDBOX 9  	/* TREE */
+#define MKNAME 2  	/* OBJECT in TREE #9 */
+#define MKOK 3  	/* OBJECT in TREE #9 */
+#define MKCNCL 4  	/* OBJECT in TREE #9 */
+#define ADSEARCH 10  	/* TREE */
+#define SFNAME 2  	/* OBJECT in TREE #10 */
+#define SFOK 3  	/* OBJECT in TREE #10 */
+#define SFCANCEL 4  	/* OBJECT in TREE #10 */
+#define ADPRINT 11  	/* TREE */
+#define PRNAME 2  	/* OBJECT in TREE #11 */
+#define ADSETPRE 12  	/* TREE */
+#define SPTITLE 1  	/* OBJECT in TREE #12 */
+#define SPCDYES 4  	/* OBJECT in TREE #12 */
+#define SPCDNO 5  	/* OBJECT in TREE #12 */
+#define SPCCYES 8  	/* OBJECT in TREE #12 */
+#define SPCCNO 9  	/* OBJECT in TREE #12 */
+#define SPCOWYES 12  	/* OBJECT in TREE #12 */
+#define SPCOWNO 13  	/* OBJECT in TREE #12 */
+#define SPDC1 16  	/* OBJECT in TREE #12 */
+#define SPDC2 17  	/* OBJECT in TREE #12 */
+#define SPDC3 18  	/* OBJECT in TREE #12 */
+#define SPDC4 19  	/* OBJECT in TREE #12 */
+#define SPDC5 20  	/* OBJECT in TREE #12 */
+#define SPTF_DEF 23  	/* OBJECT in TREE #12 */
+#define SPTF12HR 24  	/* OBJECT in TREE #12 */
+#define SPTF24HR 25  	/* OBJECT in TREE #12 */
+#define SPDF_DEF 28  	/* OBJECT in TREE #12 */
+#define SPDFMMDD 29  	/* OBJECT in TREE #12 */
+#define SPDFDDMM 30  	/* OBJECT in TREE #12 */
+#define SPOK 31  	/* OBJECT in TREE #12 */
+#define SPCNCL 32  	/* OBJECT in TREE #12 */
+#define ADFMASK 13  	/* TREE */
+#define FMMASK 2  	/* OBJECT in TREE #13 */
+#define FMOK 3  	/* OBJECT in TREE #13 */
+#define FMCANCEL 4  	/* OBJECT in TREE #13 */
+#define ADDESKCF 14  	/* TREE */
+#define DCDEFAPP 4  	/* OBJECT in TREE #14 */
+#define DCDEFWIN 5  	/* OBJECT in TREE #14 */
+#define DCPMFULL 8  	/* OBJECT in TREE #14 */
+#define DCPMFILE 9  	/* OBJECT in TREE #14 */
+#define DCFUNBOX 11  	/* OBJECT in TREE #14 */
+#define DC_IBOX 12  	/* OBJECT in TREE #14 */
+#define DCFUNPRV 13  	/* OBJECT in TREE #14 */
+#define DCFUNNXT 14  	/* OBJECT in TREE #14 */
+#define DCFUNNUM 15  	/* OBJECT in TREE #14 */
+#define DCFUNLT 16  	/* OBJECT in TREE #14 */
+#define DCFUNPTH 17  	/* OBJECT in TREE #14 */
+#define DCFUNRT 18  	/* OBJECT in TREE #14 */
+#define DCMNUBOX 20  	/* OBJECT in TREE #14 */
+#define DCMNUTXT 22  	/* OBJECT in TREE #14 */
+#define DCMNUPRV 24  	/* OBJECT in TREE #14 */
+#define DCMNUNXT 25  	/* OBJECT in TREE #14 */
+#define DCMNUKEY 26  	/* OBJECT in TREE #14 */
+#define DCMNUCLR 27  	/* OBJECT in TREE #14 */
+#define DCFREMEM 28  	/* OBJECT in TREE #14 */
+#define DCOK 29  	/* OBJECT in TREE #14 */
+#define DC_CNCL 30  	/* OBJECT in TREE #14 */
+#define ADTTREZ 15  	/* TREE */
+#define TTREZSTL 3  	/* OBJECT in TREE #15 */
+#define TTREZOK 8  	/* OBJECT in TREE #15 */
+#define TTREZCAN 9  	/* OBJECT in TREE #15 */
+#define ADFALREZ 16  	/* TREE */
+#define FREZTEXT 4  	/* OBJECT in TREE #16 */
+#define FREZLIST 5  	/* OBJECT in TREE #16 */
+#define FREZNAME 24  	/* OBJECT in TREE #16 */
+#define FREZOK 29  	/* OBJECT in TREE #16 */
+#define FREZCAN 30  	/* OBJECT in TREE #16 */
+#define ADINSWIN 17  	/* TREE */
+#define IW_NAME 2  	/* OBJECT in TREE #17 */
+#define IW_RBOX 4  	/* OBJECT in TREE #17 */
+#define IW_FOLD 5  	/* OBJECT in TREE #17 */
+#define IW_FILE 6  	/* OBJECT in TREE #17 */
+#define IW_IBOX 7  	/* OBJECT in TREE #17 */
+#define IW_ICON 8  	/* OBJECT in TREE #17 */
+#define IW_UP 9  	/* OBJECT in TREE #17 */
+#define IW_DOWN 10  	/* OBJECT in TREE #17 */
+#define IW_INST 12  	/* OBJECT in TREE #17 */
+#define IW_REMV 13  	/* OBJECT in TREE #17 */
+#define IW_SKIP 14  	/* OBJECT in TREE #17 */
+#define IW_CNCL 15  	/* OBJECT in TREE #17 */
+#define ADAMIREZ 18  	/* TREE */
+#define AMIREZ0 4  	/* OBJECT in TREE #18 */
+#define AMREZOK 18  	/* OBJECT in TREE #18 */
+#define AMREZCAN 19  	/* OBJECT in TREE #18 */
+#define ADFORMAT 19  	/* TREE */
+#define FMT_DRVA 4  	/* OBJECT in TREE #19 */
+#define FMT_DRVB 5  	/* OBJECT in TREE #19 */
+#define FMTLABEL 6  	/* OBJECT in TREE #19 */
+#define FMT_SS 8  	/* OBJECT in TREE #19 */
+#define FMT_DS 9  	/* OBJECT in TREE #19 */
+#define FMT_HD 10  	/* OBJECT in TREE #19 */
+#define FMT_BAR 11  	/* OBJECT in TREE #19 */
+#define FMT_OK 12  	/* OBJECT in TREE #19 */
+#define FMT_CNCL 13  	/* OBJECT in TREE #19 */
+#define ADBKGND 20  	/* TREE */
+#define BGDESK 4  	/* OBJECT in TREE #20 */
+#define BGWIN 5  	/* OBJECT in TREE #20 */
+#define BGPAT0 9  	/* OBJECT in TREE #20 */
+#define BGPAT7 16  	/* OBJECT in TREE #20 */
+#define BGSAMPLE 17  	/* OBJECT in TREE #20 */
+#define BGCOL0 19  	/* OBJECT in TREE #20 */
+#define BGCOL15 34  	/* OBJECT in TREE #20 */
+#define BGOK 35  	/* OBJECT in TREE #20 */
+#define BGCANCEL 36  	/* OBJECT in TREE #20 */
+#define BGCANCEL 36  	/* OBJECT in TREE #20 */
+#define BGCANCEL 36  	/* OBJECT in TREE #20 */
+#define BGCANCEL 36  	/* OBJECT in TREE #20 */
+#define BGCANCEL 36  	/* OBJECT in TREE #20 */
+#define BGCANCEL 36  	/* OBJECT in TREE #20 */
+#define BGCANCEL 36  	/* OBJECT in TREE #20 */
+#define BGCANCEL 36  	/* OBJECT in TREE #20 */
+#define BGCANCEL 36  	/* OBJECT in TREE #20 */
+#define BGCANCEL 36  	/* OBJECT in TREE #20 */
+#define BGCANCEL 36  	/* OBJECT in TREE #20 */
+#define BGCANCEL 36  	/* OBJECT in TREE #20 */
+#define BGCANCEL 36  	/* OBJECT in TREE #20 */
+#define BGCANCEL 36  	/* OBJECT in TREE #20 */
+#define BGCANCEL 36  	/* OBJECT in TREE #20 */
+#define BGCANCEL 36  	/* OBJECT in TREE #20 */
+#define BGCANCEL 36  	/* OBJECT in TREE #20 */
+#define BGCANCEL 36  	/* OBJECT in TREE #20 */
+#define BGCANCEL 36  	/* OBJECT in TREE #20 */
+#define BGCANCEL 36  	/* OBJECT in TREE #20 */
+#define BGCANCEL 36  	/* OBJECT in TREE #20 */
+#define BGCANCEL 36  	/* OBJECT in TREE #20 */
+#define STRESOL 21  	/* STRING */
+#define STSHOW 22  	/* STRING */
+#define STPRTERR 23  	/* STRING */
+#define STNOAPPL 24  	/* STRING */
+#define STNOWIND 25  	/* STRING */
+#define STILLDIR 26  	/* STRING */
+#define STILLOP 27  	/* STRING */
+#define STABORT 28  	/* STRING */
+#define STDELDIS 29  	/* STRING */
+#define STAPGONE 30  	/* STRING */
+#define STFOFAIL 31  	/* STRING */
+#define STDISKFU 32  	/* STRING */
+#define STFO8DEE 33  	/* STRING */
+#define STDEEPPA 34  	/* STRING */
+#define STTRINFO 35  	/* STRING */
+#define STPRINFO 36  	/* STRING */
+#define STNODRA1 37  	/* STRING */
+#define STSVINF 38  	/* STRING */
+#define STRMVDEL 39  	/* STRING */
+#define STINVKEY 40  	/* STRING */
+#define STDUPKEY 41  	/* STRING */
+#define STNOMEM 42  	/* STRING */
+#define STRMVLOC 43  	/* STRING */
+#define STICNTYP 44  	/* STRING */
+#define STNOMTCH 45  	/* STRING */
+#define STFMTERR 46  	/* STRING */
+#define STFMTINF 47  	/* STRING */
+#define STNODRIV 48  	/* STRING */
+#define STDRIVID 49  	/* STRING */
+#define STDELFIL 50  	/* STRING */
+#define STDELDIR 51  	/* STRING */
+#define STMAFAIL 52  	/* STRING */
+#define STFILENF 53  	/* STRING */
+#define STOPFAIL 54  	/* STRING */
+#define STCRTFIL 55  	/* STRING */
+#define STRDFILE 56  	/* STRING */
+#define STWRFILE 57  	/* STRING */
+#define STRENAME 58  	/* STRING */
+#define STDEFDIR 59  	/* STRING */
+#define STINVINF 60  	/* STRING */
+#define STCNSRCH 61  	/* STRING */
+#define STNOMORE 62  	/* STRING */
+#define STDUPCUT 63  	/* STRING */
